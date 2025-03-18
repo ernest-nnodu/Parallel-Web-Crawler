59e2198d9893faa3d4a90156cd9bc6e9caf04e13
ernest nnodu <ernest.nnodu@outlook.com>
2025-03-18 12:43:04 +0000

    Implement performance profiler -- used dynamic proxy to implement profiling

--- src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,9 +3,16 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
@@ -33,13 +40,34 @@ final class ProfilerImpl implements Profiler {
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
 
-    return delegate;
+    if (!klass.isInterface()) {
+      throw new IllegalArgumentException("Interface required for dynamic proxy");
+    }
+
+    boolean hasProfiled = Arrays.stream(klass.getMethods())
+            .anyMatch(method -> method.getAnnotation(Profiled.class) != null);
+
+    if (!hasProfiled) {
+      throw new IllegalArgumentException("Interface should contain a @Profiled method");
+    }
+
+      return klass.cast(Proxy.newProxyInstance(
+              klass.getClassLoader(),
+              new Class[]{klass},
+              new ProfilingMethodInterceptor(clock, delegate, state)
+      ));
   }
 
   @Override
   public void writeData(Path path) {
     // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
+    try(Writer writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8,
+            StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+      writeData(writer);
+    } catch (IOException ex) {
+       System.out.println(ex.getMessage());
+    }
   }
 
   @Override
